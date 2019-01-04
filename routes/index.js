4acb70c597f4d196b5adbafd1939e30d4e1a9345
uhhi2000 <32762138+uhhi2000@users.noreply.github.com>
2019-01-04 21:59:11 +0100

    Setup + index
    Converted from pug to handlebars engine. Set up project and made index view using bootstrap.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Examensarbetet' });
+  res.render('product-page/index', { title: 'Express' });
 });
 
 module.exports = router;
