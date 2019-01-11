e4ac067762905d544a5d7fa6ac384e9cea47ca90
uhhi2000 <32762138+uhhi2000@users.noreply.github.com>
2019-01-11 01:05:30 +0100

    Product cards more responsiv

--- transfer/product-list.js
+++ transfer/product-list.js
@@ -9,19 +9,19 @@ var products = [
         new Product({
         imagePath: 'https://images.unsplash.com/photo-1523983302122-73e869e1f850?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
         title: 'BMW',
-        text: 'For sale',
-        price: 290
+        text: 'Lorem ipsum',
+        price: 290.000
     }),
     new Product({
         imagePath: 'https://images.unsplash.com/photo-1536700503339-1e4b06520771?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
         title: 'Tesla - Model 3',
-        text: 'Already bought',
+        text: 'Lore ipsum',
         price: 500.000
     }),
     new Product({
         imagePath: 'https://images.unsplash.com/photo-1506244856291-8910ea843e81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
         title: 'Volvo',
-        text: 'Discount',
+        text: 'Vi rullar',
         price: 180.000
     })
 ];
