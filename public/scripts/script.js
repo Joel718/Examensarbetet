8b5664b9d6209ef19c4044fbc8a0f863f9f25a16
uhhi2000 <32762138+uhhi2000@users.noreply.github.com>
2019-02-07 13:30:00 +0100

    No right click
    Refuse right click on page

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -22,3 +22,70 @@ $(document).ready(function() {
         });
     }
 }
+
+(function(){if(!/*@cc_on!@*/0)return;var e = "abbr,article,aside,audio,bb,canvas,datagrid,datalist,details,dialog,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while(i--){document.createElement(e[i]);}})();
+
+if (navigator.userAgent.indexOf('Mac OS X') != -1) {
+  $("body").addClass("mac");
+} else {
+  $("body").addClass("pc");
+}
+
+var Fb = {}; //An empty object literal for holding the function
+Fb.log = function(obj, consoleMethod) {
+       if (window.console && window.console.firebug && window.console.firebug.replace(/^\s\s*/, '').replace(/\s\s*$/, '') !== '') {
+               if (typeof consoleMethod === "string" && typeof console[consoleMethod] === "function") {
+                       console[consoleMethod](obj);
+               } else {
+                       console.log(obj);
+               }
+       }
+};
+
+$('iframe').attr('src', $('iframe').attr('src'));
+
+var msg_box ="please don't right click";
+function dis_rightclickIE(){
+if (navigator.appName == 'Microsoft Internet Explorer' && (event.button == 2 || event.button == 3))
+alert(msg_box);
+}
+
+function dis_rightclickNS(e){
+if ((document.layers||document.getElementById&&!document.all) && (e.which==2||e.which==3))
+{
+alert(msg_box);
+return false;
+}
+}
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=dis_rightclickNS;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=dis_rightclickIE;
+}
+document.oncontextmenu=new Function("alert(msg_box);return false");
+
+
+
+var msg_box ="please don't right click";
+function dis_rightclickIE(){
+if (navigator.appName == 'Microsoft Internet Explorer' && (event.button == 2 || event.button == 3))
+alert(msg_box);
+}
+
+function dis_rightclickNS(e){
+if ((document.layers||document.getElementById&&!document.all) && (e.which==2||e.which==3))
+{
+alert(msg_box);
+return false;
+}
+}
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=dis_rightclickNS;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=dis_rightclickIE;
+}
+document.oncontextmenu=new Function("alert(msg_box);return false");
