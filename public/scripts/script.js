52e22c1294ed5b68346c0c1a19ec4920e3532037
uhhi2000 <32762138+uhhi2000@users.noreply.github.com>
2019-02-07 13:36:40 +0100

    Cross browser dependency

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -67,7 +67,7 @@ document.onmousedown=dis_rightclickIE;
 document.oncontextmenu=new Function("alert(msg_box);return false");
 
 
-
+// Dont right click
 var msg_box ="please don't right click";
 function dis_rightclickIE(){
 if (navigator.appName == 'Microsoft Internet Explorer' && (event.button == 2 || event.button == 3))
@@ -89,3 +89,43 @@ else if (document.all&&!document.getElementById){
 document.onmousedown=dis_rightclickIE;
 }
 document.oncontextmenu=new Function("alert(msg_box);return false");
+
+// Cross browser dependency-free dom ready
+var DOMReady = function(a, b, c) {
+        b = document;
+        c = 'addEventListener';
+        b[c] ? b[c]('DocumentContentLoaded', a) : window.attachEvent('onload', a);
+      };
+          
+      DOMReady(function () {
+        alert('The DOM is Ready!');
+});
+
+(function(win, doc){
+	if(win.addEventListener)return;		//No need to polyfill
+
+	function docHijack(p){var old = doc[p];doc[p] = function(v){return addListen(old(v))}}
+	function addEvent(on, fn, self){
+		return (self = this).attachEvent('on' + on, function(e){
+			var e = e || win.event;
+			e.preventDefault  = e.preventDefault  || function(){e.returnValue = false}
+			e.stopPropagation = e.stopPropagation || function(){e.cancelBubble = true}
+			fn.call(self, e);
+		});
+	}
+	function addListen(obj, i){
+		if(i = obj.length)while(i--)obj[i].addEventListener = addEvent;
+		else obj.addEventListener = addEvent;
+		return obj;
+	}
+
+	addListen([doc, win]);
+	if('Element' in win)win.Element.prototype.addEventListener = addEvent;			//IE8
+	else{																			//IE < 8
+		doc.attachEvent('onreadystatechange', function(){addListen(doc.all)});		//Make sure we also init at domReady
+		docHijack('getElementsByTagName');
+		docHijack('getElementById');
+		docHijack('createElement');
+		addListen(doc.all);	
+	}
+})(window, document);
